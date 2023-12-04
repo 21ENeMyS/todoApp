e87257ff7343962235c61fe8d817bf19a898c17b
Almarup <delltop231@gmail.com>
2023-12-04 08:31:16 +0700

    Menambahkan dari sisi client atau front end menggunakan Tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import WithoutNav from "./assets/components/WithoutNav";
+import WithNav from "./assets/components/WithNav";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route element={<WithNav />}>
+            <Route exact path="/" Component={Home} />
+            <Route path="/about" Component={About} />
+          </Route>
+          <Route element={<WithoutNav />}>
+            <Route path="/signin" Component={Signin} />
+            <Route path="/signup" Component={Signup} />
+          </Route>
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ["poppins", ...defaultTheme.fontFamily.sans],
+        body: ["roboto", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        background: "	#FFFFFF",
+        highlight: "#F0F0FF",
+        second: "#42A5C4",
+        button: "#E8505C",
+        primary: "#3E89CB",
+      },
+    },
   },
   plugins: [],
 };
